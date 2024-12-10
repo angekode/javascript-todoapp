8b05f5f9c6010ed4a87688b4f2517e2ca5b3c661
Nicolas Gigot <nicolas.gigot@protonmail.com>
2024-12-10 06:58:11 +0100

    Modif: Nom des éléments, gestion d'erreur.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-
-
 // Evènements
 
 const userInput = document.getElementById("user_input");
@@ -39,38 +37,46 @@ if (buttonRemove == null) {
 var lastId = -1;
 
 function addTask() {
-    const listElement = document.querySelector("ol#list");
-    const userInput = document.querySelector("input");
 
-    if (listElement != null && userInput != null) {
-        lastId++;
-
-        const newLiElement = document.createElement("li");
-        listElement.appendChild(newLiElement);
-        
-        const newCheckboxElement = document.createElement("input");
-        newCheckboxElement.setAttribute("id",lastId);
-        newCheckboxElement.setAttribute("type","checkbox");
-        newLiElement.appendChild(newCheckboxElement);
-
-        const newLabelElement = document.createElement("label");
-        newLabelElement.setAttribute("for",lastId); // permet d'agir sur la checkbox quand on clique sur le texte
-        newLabelElement.innerText = userInput.value;
-        newLiElement.appendChild(newLabelElement);
-
-        const newCrossElement = document.createElement("img");
-        newCrossElement.setAttribute("src","cross.svg");
-        newCrossElement.setAttribute("class","img_cross");
-        newCrossElement.addEventListener("click",() => { newLiElement.remove(); });
-        newLiElement.appendChild(newCrossElement);
+    const listElement = document.getElementById("task_list");
+    if (listElement == null) {
+        console.log("Liste des tâches manquante");
+        return;
     }
+
+    const userInput = document.getElementById("user_input");
+    if (userInput == null) {
+        console.log("Champe de saisie manquant");
+        return;
+    }
+    
+    lastId++;
+
+    const newLiElement = document.createElement("li");
+    listElement.appendChild(newLiElement);
+    
+    const newCheckboxElement = document.createElement("input");
+    newCheckboxElement.setAttribute("id",lastId);
+    newCheckboxElement.setAttribute("type","checkbox");
+    newLiElement.appendChild(newCheckboxElement);
+
+    const newLabelElement = document.createElement("label");
+    newLabelElement.setAttribute("for",lastId); // permet d'agir sur la checkbox quand on clique sur le texte
+    newLabelElement.innerText = userInput.value;
+    newLiElement.appendChild(newLabelElement);
+
+    const newCrossElement = document.createElement("img");
+    newCrossElement.setAttribute("src","cross.svg");
+    newCrossElement.setAttribute("class","img_cross");
+    newCrossElement.addEventListener("click",() => { newLiElement.remove(); });
+    newLiElement.appendChild(newCrossElement);
 }
 
 function removeCheckedTasks() {
 
-    const listElement = document.querySelector("ol#list");
+    const listElement = document.getElementById("task_list");
     if (listElement == null) {
-        console.log("ol#list inexistant");
+        console.log("Liste de tâches manquantes");
         return;
     }
 
